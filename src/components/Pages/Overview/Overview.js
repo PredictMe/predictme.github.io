93a16d46419b7c9098f741c0c8326fbd61a03012
David Ilievski <id@Davids-MacBook-Air.local>
2022-08-25 21:25:30 +0200

    update

--- src/components/Pages/Overview/Overview.js
+++ src/components/Pages/Overview/Overview.js
@@ -47,7 +47,7 @@ function Overview() {
 
 const Text = {
   title01 : "How it works",
-  descrition01 : "When you buy a prediction, the dapp is executed by a network of workers. It fetches new data from binance and runs a machine learning algorithm to predict the market trend.",
+  descrition01 : "When you buy a prediction, the dapp is executed by a network of workers. It fetches data from binance and runs a machine learning algorithm to predict the market trend.",
   title02 : "The AI model is secret at all times.",
   descrition02 : "The model is stored encrypted. It is only decrypted inside the worker in a sgx enclave. The sgx enclave makes it imposible for the hardware owner to inspect the code.",
 
