42c227fed039557072a96a34877965e8ffbbf2ff
ilievski-david <ilievskidavid97@gmail.com>
2022-10-20 00:28:51 +0200

    refactor

--- src/components/Pages/Docs/Docs.js
+++ src/components/Pages/Docs/Docs.js
@@ -6,35 +6,25 @@ export default class Docs extends Component {
   render() {
     return (
       <div className='docs-container'>
-        <div className='docs-layout-01'>
-          <h1>How to monetize AI using Iexec</h1>
-          <div className='docs-box-01'>
-              <div className='docs-01-number'>1</div>
-              <div className='docs-01-text'><div>Train a machine learning model</div></div>
+        <div className='docs-layout'>
+          <div className='docs-title'>How to monetize AI using Iexec?</div>
+          <div className='docs-box'>
+              <div className='docs-number'>1</div>
+              <div className='docs-text'><div>Train a machine learning model</div></div>
           </div>
-          <div className='docs-box-01'>
-             <div className='docs-01-number'>2</div>
-             <div className='docs-01-text'><div>Create a docker image with the ml model (<a href='https://github.com/PredictMe/predictme-ml' target="_blank"> link </a>)</div> </div>
+          <div className='docs-box'>
+             <div className='docs-number'>2</div>
+             <div className='docs-text'><div>Create a docker image with the ml model (<a className='docs-link' href='https://github.com/PredictMe/predictme-ml' target="_blank"> link </a>)</div> </div>
           </div>
-          <div className='docs-box-01'>
-             <div className='docs-01-number'>3</div>
-             <div className='docs-01-text'><div>Create an iexec dapp with the docker image (<a href='https://docs.iex.ec/for-developers/your-first-app' target="_blank"> link </a>)</div> </div>
+          <div className='docs-box'>
+             <div className='docs-number'>3</div>
+             <div className='docs-text'><div>Create an iexec dapp with the docker image (<a className='docs-link' href='https://docs.iex.ec/for-developers/your-first-app' target="_blank"> link </a>)</div> </div>
           </div>
-          <div className='docs-box-01'>
-             <div className='docs-01-number'>4</div>
-             <div className='docs-01-text'><div>Publish app orders (<a href='https://docs.iex.ec/for-developers/advanced/manage-your-apporders' target="_blank"> link </a>)</div> </div>
+          <div className='docs-box'>
+             <div className='docs-number'>4</div>
+             <div className='docs-text'><div>Publish app orders (<a className='docs-link' href='https://docs.iex.ec/for-developers/advanced/manage-your-apporders' target="_blank"> link </a>)</div> </div>
           </div>
         </div>
-        {/*
-        <div className='docs-layout-02'>
-          <div><img className='docs-02-github-logo' src='./github-white.png'></img><h3 className='docs-02-github-link-text'>PredictMe Docker Image</h3></div>
-          <div><h3 className='docs-02-github-link-text'>PredictMe Webapp</h3></div>
-          <div><h3 className='docs-02-github-link-text'>Iexec javascript sdk</h3></div>
-          <h3>_</h3>
-          <h3>How to create an Iexec dapp</h3>
-          <h3>How to create an encrypted Iexec dapp</h3>
-        </div>
-        */}
       </div>
     )
   }
